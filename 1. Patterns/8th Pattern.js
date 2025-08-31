dd18a5bc7c38759af3b7746e258765a62433bd5e
Harsh Aghera <harsh.aghera2@gmail.com>
2025-08-31 10:42:44 +0530

    Added new pattern.

--- 1. Patterns/8th Pattern.js
+++ 1. Patterns/8th Pattern.js
@@ -1,5 +1,14 @@
 /* 
 
+    *    
+
+   ***   
+
+  *****  
+
+ ******* 
+
+*********
 *********
 
  ******* 
@@ -15,6 +24,20 @@
 //Print above given pattern
 
 const n = 5;
+for (i = 0; i < n; i++) {
+  let result = '';
+  for (j = 0; j < n - i - 1; j++) {
+    result += ' ';
+  }
+  for (j = 0; j < 2 * i + 1; j++) {
+    result += '*';
+  }
+  for (j = 0; j < n - i - 1; j++) {
+    result += ' ';
+  }
+  result += '\n';
+  console.log(result);
+}
 for (i = 0; i < n; i++) {
   let result = '';
   for (j = 0; j < i; j++) {
