a58e14546c535f0bf62094e5afabad4d870631e8
Harsh Aghera <harsh.aghera2@gmail.com>
2025-11-19 07:51:15 +0530

    Added factory methods for LinkedList.

--- 6. Linked List/1. coreMethods.js
+++ 6. Linked List/1. coreMethods.js
@@ -24,6 +24,54 @@ class LinkedList {
     this.length++;
     return this;
   }
+
+  pop() {
+    if (!this.head) {
+      return undefined;
+    }
+    let temp = this.head;
+    let pre = this.head;
+    while (temp.next) {
+      pre = temp;
+      temp = temp.next;
+    }
+    this.tail = pre;
+    this.tail.next = null;
+    this.length--;
+    if (this.length == 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return temp;
+  }
+
+  unshift(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  shift() {
+    if (!this.head) {
+      return undefined;
+    } else {
+      const temp = this.head;
+      this.head = temp.next;
+      temp.next = null;
+      this.length--;
+      if (this.length == 0) {
+        this.tail = null;
+      }
+      return temp;
+    }
+  }
 }
 
 let myLinkedList = new LinkedList(7);
